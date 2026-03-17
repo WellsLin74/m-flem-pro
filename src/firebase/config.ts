a23ce5a57031f0aa0c1ae85e8aadb18948d422c5
Wells Lin <Wells.lw.Lin@gmail.com>
2026-03-17 08:38:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6149430667-dcafe",
+  "appId": "1:492472687669:web:984946a276a2441ba5257c",
+  "apiKey": "AIzaSyAUp12z5eMICdFqncasInWxK7_xGWBwO2I",
+  "authDomain": "studio-6149430667-dcafe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "492472687669"
+};
